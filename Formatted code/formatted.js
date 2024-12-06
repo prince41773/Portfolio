31bef888b7124efaf691958adfabb5d0f759dfa8
Prince Raiyani <116511694+prince41773@users.noreply.github.com>
2024-12-06 15:41:20 +0530

    Update formatted.js

--- Formatted code/formatted.js
+++ Formatted code/formatted.js
@@ -76,7 +76,7 @@ navLi.forEach( li => {
   }
 })
 })
-console.log('%c Designed and Developed by Vinod Jangid ', 'background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;');
+console.log('%c Designed and Developed by Prince Raiyani ', 'background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;');
 
 
 
@@ -100,10 +100,6 @@ function scrolltoTopfunction(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-// document.addEventListener("contextmenu", function (e){
-//   e.preventDefault();
-// }, false);
-// https://github.com/vinodjangid07
 document.addEventListener("contextmenu", function(e){
   if (e.target.nodeName === "IMG") {
       e.preventDefault();
